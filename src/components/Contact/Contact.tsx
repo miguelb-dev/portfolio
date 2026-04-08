24700829a9174ffde281dc544546157feced5970
miguelb-dev <mbethancourt2005@gmail.com>
2026-04-08 17:20:05 -0400

    fix(Contact): Corregir la ventana modal

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,8 +1,10 @@
 import { useRef, useState } from "react";
+import { createPortal } from "react-dom";
 import styles from "./Contact.module.css";
 import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  /* Variables de estado del formulario */
   const formRef = useRef<HTMLFormElement>(null);
   const [isSending, setIsSending] = useState(false);
   const [modal, setModal] = useState<{
@@ -15,6 +17,7 @@ const Contact = () => {
     text: "",
   });
 
+  /* Envío del formulario al correo */
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsSending(true);
@@ -51,6 +54,65 @@ const Contact = () => {
     setModal((prev) => ({ ...prev, isOpen: false }));
   };
 
+  /* Ventana Modal */
+  const renderModal = () => {
+    if (!modal.isOpen) return null;
+
+    return createPortal(
+      <div className={styles["modal-overlay"]} onClick={closeModal}>
+        <div className={styles["message"]} onClick={(e) => e.stopPropagation()}>
+          <div
+            className={`${styles["message__icon"]} ${styles[`${modal.type}--color`]}`}
+          >
+            {modal.type === "success" ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.1"
+                stroke="currentColor"
+                className="size-6"
+                width="120"
+                height="120"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.1"
+                stroke="currentColor"
+                className="size-6"
+                width="110"
+                height="110"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+            )}
+          </div>
+
+          <p className={styles["message__text"]}>{modal.text}</p>
+
+          <button className={styles["message__button"]} onClick={closeModal}>
+            Cerrar
+          </button>
+        </div>
+      </div>,
+      document.body,
+    );
+  };
+
+  /* Código del componente de Contacto */
   return (
     <article id="contact" className="reveal-animation">
       <h2 className={styles["subtitle"]}>
@@ -60,63 +122,8 @@ const Contact = () => {
         <span style={{ color: "grey" }}> &gt;</span>{" "}
       </h2>
 
-      {/* Mensaje del formulario */}
-      {modal.isOpen && (
-        <div className={styles["modal-overlay"]} onClick={closeModal}>
-          <div
-            className={styles["message"]}
-            onClick={(e) => e.stopPropagation()}
-          >
-            <div
-              className={`${styles["message__icon"]} ${styles[`${modal.type}--color`]}`}
-            >
-              {modal.type === "success" ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.1"
-                  stroke="currentColor"
-                  className="size-6"
-                  width="120"
-                  height="120"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.1"
-                  stroke="currentColor"
-                  className="size-6"
-                  width="110"
-                  height="110"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                  />
-                </svg>
-              )}
-            </div>
+      {renderModal()}
 
-            <p className={styles["message__text"]}>{modal.text}</p>
-
-            <button className={styles["message__button"]} onClick={closeModal}>
-              Cerrar
-            </button>
-          </div>
-        </div>
-      )}
-
-      {/* Formulario */}
       <form
         ref={formRef}
         className={styles["contact-form"]}
